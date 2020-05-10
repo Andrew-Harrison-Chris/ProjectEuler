0566f72312767dfc2cbb34f67aee7f604e829ff1
Andrew Tuma <tuma.andrew@gmail.com>
2020-05-09 19:32:58 -0500

    Q2 in C++

--- Q2/EulerQ2.cpp
+++ Q2/EulerQ2.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main () {
 
+  int x1 = 0;
+  int x2 = 1;
+  int x3, sum = 0;
 
+  x3 = x1+x2;
+  
+  while (x3 <= 4000000) {
+    cout << x3 <<endl;
+    x1 = x2;
+    x2 = x3;
+    if (x3%2 == 0) {
+      sum += x3;
+    }
+    x3 = x1+x2;
 
-return 0;
+  }
+  cout << "The sum equals: " << sum << endl;
+  return 0;
 }
