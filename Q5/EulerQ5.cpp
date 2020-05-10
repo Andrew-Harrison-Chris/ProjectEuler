c3901d81dda26360a55b128bd062e27abc4745b1
Andrew Tuma <tuma.andrew@gmail.com>
2020-05-10 02:31:28 -0500

    Euler Q5 c++

---
+++ Q5/EulerQ5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int gcd(int a, int b);
+long long lcm(long long n);
+
+
+int main () {
+
+  long long n = 20;
+
+  cout << lcm(n) << endl;
+  
+  return 0;
+}
+int gcd(int a, int b) {
+  
+  return b == 0 ? a : gcd(b, a%b);
+}
+
+long long lcm(long long n) {
+
+  long long ans = 1;
+  for (long long i = 1; i<= n; i++)
+    ans = (ans * i)/(gcd(ans,i));
+    return ans;
+}
+
+
