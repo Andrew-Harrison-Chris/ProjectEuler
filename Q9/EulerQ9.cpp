b9f34abcef08503d4deb63c05ea1c4a1a7992edf
Andrew Tuma <tuma.andrew@gmail.com>
2020-05-11 17:28:00 -0500

    Q9 in c++

--- Q9/EulerQ9.cpp
+++ Q9/EulerQ9.cpp
@@ -8,11 +8,7 @@ int main () {
     for (int b=1; b<1000; b++) {
       for (int c=1; c<1000; c++) {
 	if (a*a + b*b == c*c && a+b+c == 1000) {
-	  cout << "a: " << a  << endl;
-	  cout << "b: " << b  << endl;
-	  cout << "c: " << c  << endl;
-	  cout << "a + b + c = " << a+b+c << endl;
-	  cout << "a * b * c = " << a*b*c << endl;
+	  cout << a*b*c << endl;
 	  exit(0);
 	}
       }
