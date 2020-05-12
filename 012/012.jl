902df14a2fb6f7c28f3b1f54def1055931050eb1
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2020-05-12 15:31:38 -0500

    problem 12

---
+++ 012/012.jl
@@ -0,0 +1,31 @@
+function factors(num)
+    #factors come in pairs. Check up to sqrt, then divide num
+    #by that new found factor to find other factor.
+
+    factors = []
+    for i in 1:floor(sqrt(num))
+        if mod(num,i) == 0
+            append!(factors, i)
+            if i != num/i
+                append!(factors, num/i)
+            end
+        end
+    end
+    return factors
+end
+
+function problem012()
+    triangle_num = 1
+    i = 1
+    while true
+        num_factors = length(factors(triangle_num))
+        println("Triangle Number:", triangle_num, "with # factors:", num_factors)
+        if num_factors >= 500
+            break
+        end
+        i = i+1
+        triangle_num = triangle_num + i
+    end
+    return triangle_num
+end
+problem012()
