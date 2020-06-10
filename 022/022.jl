b2adcd8e3910264d2ac3cd4d7907cca4f3a939cd
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2020-06-10 16:44:23 -0500

    Problem 22

--- 022/022.jl
+++ 022/022.jl
@@ -1,14 +1,21 @@
+using DelimitedFiles, Test
+
 function problem022()
-    file = read("/Users/chris/Desktop/ProjectEuler/022/names.txt", String)
+    files = readdlm("/Users/chris/Desktop/ProjectEuler/022/names.txt", ',', String, '\n')
+    sort!(files, dims=2)
 
+    @test files[938] == "COLIN"
 
-    bool=false #are we in a name?
-    w =3
-    for i in 1:length(w)
-
-        if file[i] == '\"'
+    solution = 0
+    for i in 1:length(files)
+        name_sum = 0
+        for j in 1:length(files[i])
+            name_sum += parse(Int, files[i][j], base=62)-9
         end
-
+        solution += name_sum*i
     end
-    w
+    
+    return solution
 end
+
+problem022()
